101e9ec4d9f92007b1d87a5c756a8f46cfe5c4b6
melnyczuk <h.melnyczuk@gmail.com>
2020-08-28 12:04:16 +0100

    🚧

--- src/api.py
+++ src/api.py
@@ -2,20 +2,20 @@ import os
 import requests
 from dataclasses import dataclass
 
-from typing import List
+from typing import List, TypedDict
 from requests import Response
 
 ROD_URL = os.getenv("ROD_URL", "localhost:9891")
-URL = f"{ROD_URL}/google"
+URL = f"{ROD_URL}/google/places"
 
 
-@dataclass
-class Place:
+@dataclass(frozen=True)
+class Place(TypedDict):
     lat: float
     lng: float
 
 
-@dataclass
+@dataclass(frozen=True)
 class API:
     def fetch_place_data(self: "API", place: Place) -> List:
         response: Response = requests.get(URL, vars(place))
