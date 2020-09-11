562a8b8ffbe98f7e4dd05b3a65d35e19123401f4
Howard Melnyczuk <h.melnyczuk@gmail.com>
2020-09-11 20:39:32 +0100

    🚚 add /ratings route (#3)
    * Add /ratings route
    
    * add type annotation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lode-stone
-simple geolocation to angle classifier via sentiment analysis
+geolocation &rarr; orientation
+![](https://upload.wikimedia.org/wikipedia/commons/2/20/Lodestone_attracting_nails.png)
+
+### Lodestone is a service that takes a geolocation and returns an angle of orientation around that point.
+
+All endpoints accept a `GET` request with a query string containing `lng` and `lat` float values. 
+
+`?lng=0.0&lat=0.0`
+
+## Routes
+| | |
+|---|---|
+| `/ratings` | uses google places ratings |
--- wsgi.py
+++ wsgi.py
@@ -1,30 +1,37 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
 from typing import Any, Tuple
+
+from src.controller import get_rated_places
 
 server = Flask(__name__)
 
 
+@server.route("/ratings", methods=["GET"])
+def _ratings() -> Tuple[str, int]:
+    get_rated_places(request.args)
+    return ("", 202)  # return id from celery task
+
+
 @server.route("/ping", methods=["GET"])
-def ping() -> Tuple[Any, int]:
+def _ping() -> Tuple[Any, int]:
     return (jsonify("pong"), 200)
 
 
 @server.errorhandler(400)
-def four_hundred(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 400)
+def _four_hundred(e: object) -> Tuple[Any, int]:
+    return (jsonify(str(e)), 400)
 
 
 @server.errorhandler(403)
-def four_oh_three(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 403)
+def _four_oh_three(e: object) -> Tuple[Any, int]:
+    return (jsonify(str(e)), 403)
 
 
 @server.errorhandler(404)
-def four_oh_four(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 404)
+def _four_oh_four(e: object) -> Tuple[Any, int]:
+    return (jsonify(str(e)), 404)
 
 
 @server.errorhandler(500)
-def five_hundred(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 500)
+def _five_hundred(e: object) -> Tuple[Any, int]:
+    return (jsonify(str(e)), 500)
