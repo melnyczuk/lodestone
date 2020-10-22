3dc1b465ea149ac5efc1c23f78ce00a9912984a9
how melnyczuk <how@melnycz.uk>
2020-10-22 18:33:50 +0100

    Add cors & standardise json response object

--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask.json import jsonify
+from flask_cors import CORS  # type: ignore
 from celery.result import AsyncResult  # type: ignore
-from typing import Any, Tuple
+from typing import Any, Tuple, Union
 
 from .celery import celery
 from .logger import logger
@@ -8,8 +10,23 @@ from .tasks import ratings
 
 server = Flask(__name__)
 celery.conf.update(server.config)
+CORS(server, origins=["*"])
 
 logger.log("lodestone is ready 🧭")
+
+
+def make_response(
+    result: Union[Any, None] = None,
+    error: Union[str, None] = None,
+    status: int = 200,
+) -> Tuple[str, int]:
+    logger.request(status)
+    if result:
+        return (jsonify({"result": result}), status)
+    if error:
+        return (jsonify({"error": error}), status)
+    else:
+        return (jsonify(), 204)
 
 
 @server.route("/ratings", methods=["POST"])
@@ -17,56 +34,49 @@ def post_ratings_geolocation() -> Tuple[str, int]:
     data = request.json
 
     if "lat" not in data or "lng" not in data:
-        logger.request(400)
-        return ("missing valid lat & lng data for this request", 400)
+        return make_response(
+            error="missing valid lat & lng data for this request",
+            status=400,
+        )
 
     result = ratings.delay(float(data["lat"]), float(data["lng"]))
 
-    logger.request(202)
-    return (jsonify(result.id), 202)
+    return make_response(result=result.id, status=202)
 
 
 @server.route("/ratings/<id>", methods=["GET"])
-def get_ratings_orientation(id: str) -> Tuple[Any, int]:
+def get_ratings_orientation(id: str) -> Tuple[str, int]:
     task = AsyncResult(id, app=celery)
 
     if not task.ready():
-        logger.request(202)
-        return (task.state, 202)
+        return make_response(error=task.state, status=202)
 
     if task.failed():
-        logger.request(500)
-        return (jsonify(str(task.result)), 500)
+        return make_response(error=task.state, status=500)
 
-    logger.request()
-    return (jsonify(task.get()), 200)
+    return make_response(result=task.result)
 
 
 @server.route("/ping", methods=["GET"])
-def ping() -> Tuple[Any, int]:
-    logger.request()
-    return (jsonify("pong"), 200)
+def ping() -> Tuple[str, int]:
+    return make_response(result="pong")
 
 
 @server.errorhandler(400)
-def four_hundred(e: object) -> Tuple[Any, int]:
-    logger.request(400)
-    return (jsonify(str(e)), 400)
+def four_hundred(e: object) -> Tuple[str, int]:
+    return make_response(error=str(e), status=400)
 
 
 @server.errorhandler(403)
-def four_oh_three(e: object) -> Tuple[Any, int]:
-    logger.request(403)
-    return (jsonify(str(e)), 403)
+def four_oh_three(e: object) -> Tuple[str, int]:
+    return make_response(error=str(e), status=403)
 
 
 @server.errorhandler(404)
-def four_oh_four(e: object) -> Tuple[Any, int]:
-    logger.request(404)
-    return (jsonify(str(e)), 404)
+def four_oh_four(e: object) -> Tuple[str, int]:
+    return make_response(error=str(e), status=404)
 
 
 @server.errorhandler(500)
-def five_hundred(e: object) -> Tuple[Any, int]:
-    logger.request(500)
-    return (jsonify(str(e)), 500)
+def five_hundred(e: object) -> Tuple[str, int]:
+    return make_response(error=str(e), status=500)
